1bd7629cfc762df418762ed418d10d2dbed42907
ColonelCipher <david910911@gmail.com>
2017-12-05 13:27:41 -0800

    Updated AI.py

--- engines/AI.py
+++ engines/AI.py
@@ -5,4 +5,6 @@ from engines import Engine
 
 class AIEngine(Engine):
     def get_move(self, board, color, move_num=None, time_remain=None, time_opponent=None):
+        moves=board.get_legal_moves(color)
+        
         
