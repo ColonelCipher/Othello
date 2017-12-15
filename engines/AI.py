3e925b9fbbda6ca6ec25574fff73bf1f13cb2e6d
ColonelCipher <david910911@gmail.com>
2017-12-15 00:52:03 -0800

    Update AI.py

--- engines/AI.py
+++ engines/AI.py
@@ -1,7 +1,6 @@
-import gym
-import tflearn
 from board import print_moves
 from engines import Engine
+from copy import deepcopy
 
 class AIEngine(Engine):
     def get_move(self, board, color, move_num=None, time_remain=None, time_opponent=None):
